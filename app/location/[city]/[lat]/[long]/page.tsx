9bc59e0703aecc594e4e308c5812f71fd94f1932
Tim Parsons <timmparsons85@gmail.com>
2023-04-24 06:48:24 -0700

    adding graphql and stepzen

---
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -0,0 +1,36 @@
+import { getClient } from '@apollo/client';
+import fetchWeatherQuery from '../../../../../graphql/queries/fetchWeatherQueries';
+import loadConfig from 'next/dist/server/config';
+
+type Props = {
+	params: {
+		city: string;
+		lat: string;
+		long: string
+	}
+}
+
+async function WeatherPage({params: { city, lat, long }}: Props) {
+	const client = getClient();
+
+	const { data } = await client.query({
+		query: fetchWeatherQuery,
+		variables: { 
+			current_weather: "true",
+			longitude: long,
+			latitude: lat,
+			timezone: 'GMT'
+		}
+	})
+
+	const results: Root = data.myQuery;
+
+	console.log('QQQ', results)
+	return (
+		<div>
+			Welcome to weather page: {city}. {lat}, {long}
+		</div>
+	)
+}
+
+export default WeatherPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+		"./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
+
   ],
   theme: {
     extend: {
